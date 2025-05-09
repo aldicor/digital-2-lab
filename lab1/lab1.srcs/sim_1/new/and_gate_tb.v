a649e7b2101f5dd4ac68a3f8c93663e5e3174ed2
aldicor <aldiazco@unal.edu.co>
2025-05-08 23:35:56 -0500

    Vivado files - lab01

---
+++ lab1/lab1.srcs/sim_1/new/and_gate_tb.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ns
+module and_gate_tb;
+
+    // Declaración de señales de prueba
+    reg input_a;    // Entrada A
+    reg input_b;    // Entrada B
+    wire y;         // Salida Y
+
+    // Instancia del módulo a probar
+    and_gate uut (
+        .input_a(input_a),
+        .input_b(input_b),
+        .y(y)
+    );
+
+    // Proceso de simulación
+    initial begin
+        // Inicializa las entradas
+        // pueden variar el tiempo de espera a los valores que requieran.
+        input_a = 0; input_b = 0; #10;  // Espera 10 ns
+        input_a = 0; input_b = 1; #10;  // Espera 10 ns
+        input_a = 1; input_b = 0; #10;  // Espera 10 ns
+        input_a = 1; input_b = 1; #10;  // Espera 10 ns
+        // Finaliza la simulación
+        $finish;
+    end
+
+    // Monitor para ver los cambios en las señales
+    initial begin
+        $monitor("Time = %t | a = %b | b = %b | y = %b", $time, input_a, input_b, y);
+    end
+
+endmodule
---
+++ lab1/lab1.srcs/sources_1/new/or_gate.v
@@ -0,0 +1,6 @@
+module or_gate(input_a, input_b, y);
+    input input_a, input_b;
+    output y;
+    assign y = input_a | input_b;
+    
+endmodule
