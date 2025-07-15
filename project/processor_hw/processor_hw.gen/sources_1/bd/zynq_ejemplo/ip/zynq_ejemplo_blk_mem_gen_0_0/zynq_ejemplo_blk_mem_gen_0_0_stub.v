f4627e16c4d729d86d8d40c175ba7c972efb7a13
aldicor <aldiazco@unal.edu.co>
2025-07-14 23:16:53 -0500

    processor template v1

---
+++ project/processor_hw/processor_hw.gen/sources_1/bd/zynq_ejemplo/ip/zynq_ejemplo_blk_mem_gen_0_0/zynq_ejemplo_blk_mem_gen_0_0_stub.v
@@ -0,0 +1,29 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2023.1 (win64) Build 3865809 Sun May  7 15:05:29 MDT 2023
+// Date        : Mon Jul 14 19:19:44 2025
+// Host        : SebasOnix-ROG-FLOW running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Xilinx/projects/project_procesador_general/project_procesador_general.gen/sources_1/bd/zynq_ejemplo/ip/zynq_ejemplo_blk_mem_gen_0_0/zynq_ejemplo_blk_mem_gen_0_0_stub.v
+// Design      : zynq_ejemplo_blk_mem_gen_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_6,Vivado 2023.1" *)
+module zynq_ejemplo_blk_mem_gen_0_0(clka, rsta, ena, wea, addra, dina, douta, rsta_busy)
+/* synthesis syn_black_box black_box_pad_pin="rsta,ena,wea[3:0],addra[31:0],dina[31:0],douta[31:0],rsta_busy" */
+/* synthesis syn_force_seq_prim="clka" */;
+  input clka /* synthesis syn_isclock = 1 */;
+  input rsta;
+  input ena;
+  input [3:0]wea;
+  input [31:0]addra;
+  input [31:0]dina;
+  output [31:0]douta;
+  output rsta_busy;
+endmodule
